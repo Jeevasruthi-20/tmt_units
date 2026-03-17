6e821b73bb2c017cf58b81034f725c842ab9ec0b
Jeevasruthi-20 <jeevasruthi20@gmail.com>
2026-03-18 00:56:53 +0530

    Final Vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vitejs.dev/config/
 export default defineConfig({
   root: path.resolve(__dirname, '..'),
+  base: '/',
   build: {
     outDir: path.resolve(__dirname, 'dist'),
     emptyOutDir: true,
